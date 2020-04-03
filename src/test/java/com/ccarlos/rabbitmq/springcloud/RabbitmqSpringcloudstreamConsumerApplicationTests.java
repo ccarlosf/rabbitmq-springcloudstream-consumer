b470280750f06bdb6cc4cf851d92f47ec3213dee
ccarlos <2238513100@qq.com>
2020-04-03 16:07:30 +0800

    RabbitMQ与Spring Cloud Stream整合 消费端项目初始化

---
+++ src/test/java/com/ccarlos/rabbitmq/springcloud/RabbitmqSpringcloudstreamConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccarlos.rabbitmq.springcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqSpringcloudstreamConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
