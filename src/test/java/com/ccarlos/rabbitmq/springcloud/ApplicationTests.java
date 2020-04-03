60b43be49f1054ed20f944f4207978a982065efc
ccarlos <2238513100@qq.com>
2020-04-03 16:26:05 +0800

    4-17 RabbitMQ与Spring Cloud Stream整合-1 4-18 RabbitMQ与Spring Cloud Stream整合-2

--- src/test/java/com/ccarlos/rabbitmq/springcloud/ApplicationTests.java
+++ src/test/java/com/ccarlos/rabbitmq/springcloud/ApplicationTests.java
@@ -1,13 +1,13 @@
 package com.ccarlos.rabbitmq.springcloud;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RabbitmqSpringcloudstreamConsumerApplicationTests {
+public class ApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
